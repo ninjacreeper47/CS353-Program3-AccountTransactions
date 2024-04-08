33f9510a7c692694ccb8eea9b3e9d3957a5a40cd
Zach morris <zwmvq2@umkc.edu>
2024-04-08 07:02:15 -0500

    before output formating

--- StatementReport.rkt
+++ StatementReport.rkt
@@ -7,8 +7,5 @@
   (let
       ([out (open-output-file output-filename #:exists 'replace)]
        [accounts (process-transactions (file->accounts) (file->transaction-lines))])
-    (write "Hello World" out)
-    (write "Test2" out)
-    (writeln "Test3" out)
-    (writeln "test4" out)
+    (write accounts out)
   (close-output-port out)))
